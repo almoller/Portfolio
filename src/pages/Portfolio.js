5823546b6073edef849c80a0526ab6d41e7b5011
almoller <moller.alan@gmail.com>
2018-08-13 17:45:27 -0600

    updated portfolio page

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -4,6 +4,8 @@ import { Container, Row, Col } from '../components/Grid';
 import Jumbotron from '../components/Jumbotron';
 import Project from '../components/Project';
 import projects from '../projects.json';
+import badge from '../images/coding-badge-UofU.png';
+
 
 class Portfolio extends Component {
     state = {
@@ -13,7 +15,7 @@ class Portfolio extends Component {
     render() {
       return (
         <Container fluid>
-        <h2 className='text-center'>Portfolio</h2>
+        <h2 className='text-center pageTitle'>Portfolio</h2>
           <Row>
             <Col size='sm-12'>
             <div className="projectSection">
@@ -34,7 +36,20 @@ class Portfolio extends Component {
           <Row>
             <Col size="lg-6">
               <Jumbotron>
-                <h1>2 Cool!</h1>
+                <Row>
+                  <Col size='sm-6'>
+                    <h1>Web Dev Skills</h1>
+                    <ul>
+                      <li>HTML5/CSS</li>
+                      <li>Javascript</li>
+                    </ul>
+                  </Col>
+                  <Col size='sm-6'>
+                    <a href="https://www.youracclaim.com/badges/659bc848-aa14-48cb-b208-aa90946c111c/public_url">
+                      <img src={badge} width="150px" height="150px" alt="badge" />
+                    </a>
+                  </Col>
+                </Row>
               </Jumbotron>
             </Col>
             <Col size="lg-6">
